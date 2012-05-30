1a014aa960b1137bbc388bea0f3c02a9070064ea
preek <alain.lafon@dispatched.ch>
2012-05-30 15:04:16 +0300

    add minimum validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :title, :isbn, :description
+
+  validates_presence_of :author, :title
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ActiveRecord::Base
   attr_accessible :name, :street, :phone, :email, :district, :is_library_visitor
+
+  validates_presence_of :name
 end
