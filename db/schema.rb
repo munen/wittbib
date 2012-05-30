32009451db8b9c8c2ec47383a22aa64df17e493a
phil <phil@branch14.org>
2012-05-30 12:37:01 +0200

    search form

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,4 @@ ActiveRecord::Schema.define(:version => 20120530093701) do
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "posts", :force => true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
 end
