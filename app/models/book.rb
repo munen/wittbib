5fbe1fe0414f9690708aad3657ea1039acf16b8f
phil <phil@branch14.org>
2012-05-30 21:05:58 +0200

    perpare for images

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
   attr_accessor :isbn
   attr_accessible :authors, :title, :isbn, :isbn_10, :isbn_13,
                   :description, :language, :page_count,
-                  :published_date, :publisher
+                  :published_date, :publisher, :image_url
 
   validates :isbn_10, :isbn_13,
     :uniqueness => true,
