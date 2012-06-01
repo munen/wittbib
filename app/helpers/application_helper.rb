0015517d1b62290065fac0e3df596e3929cd6cc2
preek <alain.lafon@dispatched.ch>
2012-06-01 19:41:04 +0300

    show boolean as icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # output is a 'check' or 'cross' symbol
+  def boolean_checkbox(condition)
+    if condition
+      "<i class='icon-ok'></i>".html_safe
+    else
+      "<i class='icon-remove'></i>".html_safe
+    end
+  end
+
 end
