1f4f68ebb6271ad1bf372dfc600b6f00c4c5aeaa
preek <alain.lafon@dispatched.ch>
2012-06-03 10:36:24 +0200

    is lending overdue?

--- app/models/lending.rb
+++ app/models/lending.rb
@@ -5,9 +5,16 @@ class Lending < ActiveRecord::Base
 
   attr_accessible :book_id, :customer_id
 
-  validates :book_id, :customer_id,
+  validates :book_id, :customer_id, :taken_at,
     :presence => true
 
   scope :not_available, where(:returned_at => nil)
+
+  # sort by when it was lend. oldest first.
+  default_scope :order => :taken_at
+
+  def is_overdue?
+    taken_at < 4.weeks.ago
+  end
 
 end
