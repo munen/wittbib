ff38bc6db5ba42bb7b5818bdd4498d2bd12d70cb
preek <alain.lafon@dispatched.ch>
2012-09-08 00:05:03 +0200

    fix default book cover

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,10 +1,10 @@
 module BooksHelper
   # default book cover if none available
   def book_cover_picture
-    if resource.image_url
-      image_tag resource.image_url
-    else
+    if resource.image_url.blank? or resource.image_url.nil?
       image_tag "default_book_icon.jpeg"
+    else
+      image_tag resource.image_url
     end
   end
 end
