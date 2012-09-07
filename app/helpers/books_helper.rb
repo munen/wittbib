4dfc5f937d0e501482f2fafc8f4bb96aa3e0ac34
preek <alain.lafon@dispatched.ch>
2012-09-07 17:15:46 +0200

    default book cover if none found on the web

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,10 @@
 module BooksHelper
+  # default book cover if none available
+  def book_cover_picture
+    if resource.image_url
+      image_tag resource.image_url
+    else
+      image_tag "default_book_icon.jpeg"
+    end
+  end
 end
