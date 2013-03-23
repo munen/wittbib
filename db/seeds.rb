a74472758199bf2f8d213bea20b551d7cd095ec3
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2013-03-23 16:43:04 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+["aml@panter.ch", "frankschneider7d@googlemail.com", "ch.smettan@gmail.com"].each do |email|
+  next if User.find_by_email email
+  User.create! :email => email, :password => "Ohf7phie"
+  puts "User created: #{email}"
+end
+
+1.upto(5).each do |i|
+  customer = "Kunde #{i}"
+  next if Customer.find_by_name customer
+  Customer.create! :name => customer
+  puts "Customer created: #{customer}"
+end
+
+1.upto(5).each do |i|
+  next if Book.find_by_title "Book #{i}"
+  Book.create! :title => "Book #{i}", :authors => "Author #{i}"
+  puts "Book created: 'Book #{i}' from 'Authors #{i}'"
+end
+
