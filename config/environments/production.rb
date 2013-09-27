8ddbe11679d90a43a4e3c35341913016b3fbdef3
Munen Alain M. Lafon <alain.lafon@dispatched.ch>
2013-09-27 18:44:09 +0200

    configure default_url for smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,5 @@ WittBib::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { :host => 'bib.wittmer-bibliothek.de' }
 end
